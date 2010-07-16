29241adf761fcd7de200bba21cbcdc56f180505d
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-16 16:22:35 -0400

    Updated the Startup

--- product/nothinbutdotnetstore/tasks/startup/Startup.cs
+++ product/nothinbutdotnetstore/tasks/startup/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using nothinbutdotnetstore.infrastructure.containers.basic;
-
 namespace nothinbutdotnetstore.tasks.startup
 {
     public class Startup
     {
-        static Dictionary<Type, DependencyFactory> factories = new Dictionary<Type, DependencyFactory>();
-
         public static void run()
         {
-//            Start.by<ConfiguringCoreServices>()
-//                .followed_by<ConfigureFronController>()
-//                .finish_by<ConfigureServiceLayer>();
+            //            Start.by<ConfiguringCoreServices>()
+            //                .followed_by<ConfigureFronController>()
+            //                .finish_by<ConfigureServiceLayer>();
         }
     }
 }
