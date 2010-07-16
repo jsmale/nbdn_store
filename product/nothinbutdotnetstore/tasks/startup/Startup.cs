a5b48fc8e733639da8577db60c3bea623e3341c4
unknown <StrubeC@.science.regn.net>
2010-07-16 15:04:48 -0400

    more refactoring

--- product/nothinbutdotnetstore/tasks/startup/Startup.cs
+++ product/nothinbutdotnetstore/tasks/startup/Startup.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 using System.Web.Compilation;
 using nothinbutdotnetstore.infrastructure.containers;
 using nothinbutdotnetstore.infrastructure.containers.basic;
@@ -10,49 +8,49 @@ using nothinbutdotnetstore.infrastructure.logging.simple;
 using nothinbutdotnetstore.tasks.stubs;
 using nothinbutdotnetstore.web.core;
 using nothinbutdotnetstore.web.core.stubs;
-using System.Linq;
 
 namespace nothinbutdotnetstore.tasks.startup
 {
-    public class Startup
-    {
-        public static void run()
-        {
-            var factories = new Dictionary<Type, DependencyFactory>();
-            configure_core_services(factories);
-            configure_front_controller(factories);
-            configure_service_layer(factories);
-        }
+	public class Startup
+	{
+		static Dictionary<Type, DependencyFactory> factories  = new Dictionary<Type, DependencyFactory>();
+		public static void run()
+		{
+			configure_core_services(factories);
+			configure_front_controller(factories);
+			configure_service_layer(factories);
+		}
+		
+		private static void addToDict<TypeToRegister>(object creation)
+		{
+			factories.Add(typeof(TypeToRegister), create_factory(creation));
+		}
 
-        static void configure_service_layer(Dictionary<Type, DependencyFactory> factories)
-        {
-            factories.Add(typeof(CatalogTasks), create_factory(new StubCatalogTasks()));
-        }
+		private static void configure_service_layer(Dictionary<Type, DependencyFactory> factories)
+		{
+			addToDict<CatalogTasks>(new StubCatalogTasks());
+//			factories.Add(typeof (CatalogTasks), create_factory());
+		}
 
-        static void configure_front_controller(Dictionary<Type, DependencyFactory> factories)
-        {
-            factories.Add(typeof(FrontController), create_factory(new DefaultFrontController(
-                                                                           new DefaultCommandRegistry(new StubFakeCommandSet())
-                                                                           )));
+		private static void configure_front_controller(Dictionary<Type, DependencyFactory> factories)
+		{
+			addToDict<FrontController>(new DefaultFrontController(new DefaultCommandRegistry(new StubFakeCommandSet())));
+			addToDict<RequestFactory>(new StubRequestFactory());
+			addToDict<ResponseEngine>(new DefaultResponseEngine(new DefaultViewFactory(new DefaultViewRegistry(null))));
 
-            factories.Add(typeof(RequestFactory), create_factory(new StubRequestFactory()));
+			DefaultViewFactory.page_factory = BuildManager.CreateInstanceFromVirtualPath;
+		}
 
-            factories.Add(typeof(ResponseEngine), create_factory(new DefaultResponseEngine(
-                                                                          new DefaultViewFactory(new DefaultViewRegistry(null)))));
+		private static void configure_core_services(Dictionary<Type, DependencyFactory> factories)
+		{
+			Container container = new BasicContainer(factories);
+			factories.Add(typeof(LoggerFactory), new BasicDependencyFactory(() => new TextWriterLoggerFactory()));
+			IOC.factory_resolver = () => container;
+		}
 
-            DefaultViewFactory.page_factory = BuildManager.CreateInstanceFromVirtualPath;
-        }
-
-        static void configure_core_services(Dictionary<Type, DependencyFactory> factories)
-        {
-            Container container = new BasicContainer(factories);
-            factories.Add(typeof(LoggerFactory),new BasicDependencyFactory(() => new TextWriterLoggerFactory()));
-            IOC.factory_resolver = () => container;
-        }
-
-        static SingletonFactory create_factory(object dependency)
-        {
-            return new SingletonFactory(new BasicDependencyFactory(() => dependency));
-        }
-    }
+		private static SingletonFactory create_factory(object dependency)
+		{
+			return new SingletonFactory(new BasicDependencyFactory(() => dependency));
+		}
+	}
 }
