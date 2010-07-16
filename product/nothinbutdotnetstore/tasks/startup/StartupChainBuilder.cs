17f75b970f9a33bf9d574b6ec1d1b1c14e738298
unknown <YoungD@.science.regn.net>
2010-07-16 17:35:03 -0400

    test for startup chain builder

--- product/nothinbutdotnetstore/tasks/startup/StartupChainBuilder.cs
+++ product/nothinbutdotnetstore/tasks/startup/StartupChainBuilder.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace nothinbutdotnetstore.tasks.startup
 {
     public class StartupChainBuilder
     {
-        
+        readonly IList<StartupCommand> startup_commands;
+        readonly StartupCommandFactory startup_command_factory;
+        readonly Type type;
+
+        public StartupChainBuilder(IList<StartupCommand> startup_commands, StartupCommandFactory startup_command_factory, Type type)
+        {
+            this.startup_commands = startup_commands;
+            this.startup_command_factory = startup_command_factory;
+            this.type = type;
+
+            startup_commands.Add(startup_command_factory.create_command_from(type));
+
+        }
     }
 }
