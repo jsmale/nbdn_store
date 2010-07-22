2e4999cfcfb7327a5c4f34a4e95f4891807479fb
unknown <yamk@.science.regn.net>
2010-07-22 14:28:23 -0400

    get department from repository

--- product/nothinbutdotnetstore/tasks/CatalogTasks.cs
+++ product/nothinbutdotnetstore/tasks/CatalogTasks.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using nothinbutdotnetstore.data_access;
 using nothinbutdotnetstore.model;
 
 namespace nothinbutdotnetstore.tasks
@@ -13,10 +14,17 @@ namespace nothinbutdotnetstore.tasks
 
 	public class DefaultCatalogTasks : CatalogTasks 
 	{
-		public IEnumerable<Department> get_all_main_departments()
-		{
-			throw new NotImplementedException();
-		}
+	    readonly Repository repository;
+
+	    public DefaultCatalogTasks(Repository repository)
+	    {
+	        this.repository = repository;
+	    }
+
+	    public IEnumerable<Department> get_all_main_departments()
+	    {
+	        return repository.get_all<Department>();
+	    }
 
 		public IEnumerable<Department> get_all_sub_departments_in(Department department)
 		{
