aee05db4c84d719cfb54599a6c85a359c9a2f39e
Jason Smale <j.smale@elsevier.com>
2010-07-22 14:37:46 -0400

    filter main departments

--- product/nothinbutdotnetstore/tasks/CatalogTasks.cs
+++ product/nothinbutdotnetstore/tasks/CatalogTasks.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using nothinbutdotnetstore.data_access;
 using nothinbutdotnetstore.model;
 
 namespace nothinbutdotnetstore.tasks
@@ -11,29 +9,4 @@ namespace nothinbutdotnetstore.tasks
         IEnumerable<Department> get_all_sub_departments_in(Department department);
         IEnumerable<Product> get_all_products_in(Department department);
     }
-
-	public class DefaultCatalogTasks : CatalogTasks 
-	{
-	    readonly Repository repository;
-
-	    public DefaultCatalogTasks(Repository repository)
-	    {
-	        this.repository = repository;
-	    }
-
-	    public IEnumerable<Department> get_all_main_departments()
-	    {
-	        return repository.get_all<Department>();
-	    }
-
-		public IEnumerable<Department> get_all_sub_departments_in(Department department)
-		{
-			throw new NotImplementedException();
-		}
-
-		public IEnumerable<Product> get_all_products_in(Department department)
-		{
-			throw new NotImplementedException();
-		}
-	}
 }
---
+++ product/nothinbutdotnetstore/tasks/DefaultCatalogTasks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using nothinbutdotnetstore.data_access;
+using nothinbutdotnetstore.model;
+
+namespace nothinbutdotnetstore.tasks
+{
+	public class DefaultCatalogTasks : CatalogTasks 
+	{
+		readonly Repository repository;
+
+		public DefaultCatalogTasks(Repository repository)
+		{
+			this.repository = repository;
+		}
+
+		public IEnumerable<Department> get_all_main_departments()
+		{
+			return repository.get_all<Department>();
+		}
+
+		public IEnumerable<Department> get_all_sub_departments_in(Department department)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Product> get_all_products_in(Department department)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
