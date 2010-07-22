20fe8c8d5d4160ccc38cee50b2a5282411ef69bf
unknown <DiVecchioJ@.science.regn.net>
2010-07-22 15:00:44 -0400

    get subdepartment test

--- product/nothinbutdotnetstore.specs/tasks/CatalogTasksSpecs.cs
+++ product/nothinbutdotnetstore.specs/tasks/CatalogTasksSpecs.cs
@@ -41,5 +41,30 @@ namespace nothinbutdotnetstore.specs.tasks
  			static Department department2;
  			static Department department3;
  		}
+
+        [Subject(typeof(DefaultCatalogTasks))]
+ 		public class when_getting_all_the_sub_departments : concern
+ 		{
+ 			Establish c = () => {
+ 			    department1 = new Department{id = 2};
+				department2 = new Department{parentId = 2};
+ 			    department3 = new Department{parentId = 2};
+ 				repository = the_dependency<Repository>();
+
+				repository.Stub(x => x.get_all<Department>())
+					.Return(new []{department1, department2, department3});
+ 			};
+ 			Because b = () =>
+ 				result = sut.get_all_sub_departments_in(department1);
+
+ 			It should_only_return_the_sub_departments = () =>
+ 				result.ShouldContainOnly(department2, department3);
+
+ 			static Repository repository;
+ 			static IEnumerable<Department> result;
+ 			static Department department1;
+ 			static Department department2;
+ 			static Department department3;
+ 		}
  	}
  }
