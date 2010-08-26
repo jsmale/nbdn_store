75a95e3bbaa50eefc99d7f6c449b7e86b97bcd47
unknown <YamK@.science.regn.net>
2010-08-26 15:00:45 -0400

    getting subdepartment

--- product/nothinbutdotnetstore/web/application/ViewDepartmentChildren.cs
+++ product/nothinbutdotnetstore/web/application/ViewDepartmentChildren.cs
@@ -1,13 +1,30 @@
 using System;
+using nothinbutdotnetstore.model;
+using nothinbutdotnetstore.tasks;
 using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.web.application
 {
     public class ViewDepartmentChildren : ApplicationCommand
     {
+        private readonly ResponseEngine responseEngine;
+        private readonly CatalogTasks catalogTasks;
+
+        public ViewDepartmentChildren(ResponseEngine responseEngine, CatalogTasks catalogTasks )
+        {
+            this.responseEngine = responseEngine;
+            this.catalogTasks = catalogTasks;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            //var hasSubDepartments = catalogTasks.department_has_sub_departments(request.map<Department>());
+            //if(hasSubDepartments)
+            //{
+                responseEngine.display(catalogTasks.get_all_sub_departments_in(request.map<Department>()));
+            //}
+           
+           
         }
     }
 }
